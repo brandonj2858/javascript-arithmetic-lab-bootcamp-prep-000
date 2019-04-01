a97d7fd91ad27a5e300c9e07917b28d2c4a6b117
Brandon James <brandonj2858@gmail.com>
2019-04-01 13:21:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ function add(a,b)
 {
   return(a + b)
 }
-function substract(a,b)
+var a = Math.floor(Math.random() * 1000)
+var b = Math.floor(Math.random() * 1000)
+function subtract(a,b)
 {
   return(a - b)
 }
